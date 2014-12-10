b365330aa030de9f3732e328b44caf32ae36c83d
Jonathan Bourne <g>
2014-12-10 11:09:47 +0100

    Shiny App
    Shiny app included current upload

---
+++ Raw/The_Word_Predictifier/server.R
@@ -0,0 +1,105 @@
+library(shiny)
+
+
+options(shiny.maxRequestSize = 15*1024^2)
+packages <- c("R.utils", "tm", "dplyr", "stringr", "xtable")
+invisible(lapply(packages, library, character.only = TRUE))
+
+#load("data/ngram_probs_stop.Rda")
+load("./data/ngramprobssmall.Rda")
+
+corpus_clean<- function(x) {
+  x <- tolower(x)
+  x <- gsub("&", "and", x)
+  x <- gsub("p m|p.m", "pm", x)
+  x <- gsub("'ll", " will", x)
+  x <- gsub("'d", " would", x)
+  x <- gsub("'m", " am", x)
+  x <- gsub("'ve", " have", x)
+  x <- gsub("'re", " are", x)
+  x <- gsub("can't|cannot|cant", "can not", x)
+  x <- gsub("n't", " not", x)
+  x <- gsub("'s", "", x)
+  x <- removeNumbers(x)
+  x <- removePunctuation(x)
+  x <- gsub(" will", "''ll", x)
+  x <- gsub(" would", "'d", x)
+  x <- gsub(" am", "'m", x)
+  x <- gsub(" have", "'ve", x)
+  x <- gsub(" are", "'re", x)
+  x <- gsub("cannot", "can't", x)
+  x <- gsub(" not", "n't", x)
+  x <- stripWhitespace(x)
+}
+
+# corpus_clean_stop <- function(x) {
+#   x <- tolower(x)
+#   x <- gsub("&", "and", x)
+#   x <- gsub("p m|p.m", "pm", x)
+#   x <- gsub("'ll", " will", x)
+#   x <- gsub("'d", " would", x)
+#   x <- gsub("'m", " am", x)
+#   x <- gsub("'ve", " have", x)
+#   x <- gsub("'re", " are", x)
+#   x <- gsub("can't|cannot|cant", "can not", x)
+#   x <- gsub("n't", " not", x)
+#   x <- gsub("'s", "", x)
+#   x <- removeNumbers(x)
+#   x <- removePunctuation(x)
+#   x <- removeWords(x, stopwords("english"))
+#   x <- stripWhitespace(x)
+# }
+
+result <- function(phrase, ngram_probs) { 
+  phrase <- str_trim(phrase, side = "both")
+  
+  wordcount <- if(length(ngram_probs) < str_count(phrase, "\\S+")) {
+    length(ngram_probs)
+  } else  {str_count(phrase, "\\S+")}
+  
+  forNgrams <- sapply(1:wordcount, function(n) word(phrase, -n, -1))
+  results <-lapply(1:wordcount, function(n) filter(ngram_probs[[n]], 
+                                                   predictor == forNgrams[[n]]))
+  results <- do.call(rbind.data.frame, results)
+  results <- ungroup(results) %>%
+    arrange( desc(Prob), desc(Ngrams))
+  results
+}
+
+
+
+shinyServer(function(input, output) {
+  
+  #phrase_stop <- corpus_clean_stop(input$phrase)
+  output$phrase1 <- renderText({
+    phrase <- corpus_clean(input$phrase)
+    #phrase_stop <- corpus_clean_stop(input$phrase)
+ 
+    results <- result(phrase, ngram_probs_small)
+#     results <- rbind(result(phrase_stop, ngram_probs_stop), result(phrase, ngram_probs)) %>%
+#       arrange( desc(Ngrams), desc(Prob))
+    results <- toupper(as.character(results[1,5]))
+  })
+  output$phrase2 <- renderText({
+    phrase <- corpus_clean(input$phrase)
+#     phrase_stop <- corpus_clean_stop(input$phrase)
+    
+    results <- result(phrase, ngram_probs_small)
+#     results <- rbind(result(phrase_stop, ngram_probs_stop), result(phrase, ngram_probs)) %>%
+#       arrange( desc(Ngrams), desc(Prob))
+    results <- as.character(results[1,5])
+    c(input$phrase, results)
+  })
+  output$view <- renderTable({ 
+    phrase <- corpus_clean(input$phrase)
+    #phrase_stop <- corpus_clean_stop(input$phrase)
+    
+    results <- result(phrase, ngram_probs_small)
+#     results <- rbind(result(phrase_stop, ngram_probs_stop), result(phrase, ngram_probs)) %>%
+#       arrange( desc(Ngrams), desc(Prob))
+    as.data.frame(results) #some sort of dplyr tablerender bug means that as.data.frame needs to be called.
+  
+  })
+  
+  
+  })
---
+++ Raw/The_Word_Predictifier/ui.R
@@ -0,0 +1,39 @@
+library(shiny)
+
+# Define UI for dataset viewer application
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Predictification"),
+  
+
+  sidebarLayout(
+    sidebarPanel( 
+      helpText("Enter the phrase you wish to predict the last word for in the box below"),
+      textInput("phrase", "Phrase to predict last word:", "My Phrase is..."),
+      submitButton("Submit"),
+      h2("Details"),
+      p("The Predictifier, comes to it's conclusions using the following method. 
+        It first cleans the sentence in two alternative ways. The first method removes all punctuation 
+        (except for those used in contractions such as the apostraphe found in \"i'd like a sandwich\") and numbers. 
+        The second method does the same and in addition removes all", 
+        a("Stop words.", href = "http://en.wikipedia.org/wiki/Stop_words")),
+      p("It then compares the cleaned phrases to two similarly cleaned dictionaries of", a("ngrams", href= "http://en.wikipedia.org/wiki/N-gram")),
+      p("matching ngrams a compiled then sorted according to Ngram length tie then ordering on length probability,
+          Thisis becuase although a lower Ngram has may have a higher probability it lacks context.
+          Finally the resulting most likely ngram is then returned. For more information click", a("here", href =""))
+    ),
+    
+    mainPanel(
+     h3(p("The Predictifier thinks your word is", align = "center")),
+      h2(textOutput("phrase1"), align = "center"),
+      p("which would make your sentence be '",strong( textOutput("phrase2", inline = TRUE)),
+        "' Does this suggestion seem sensible to you?", align = "center"),
+      h3(p("Table of alternatives")),
+      p("The options available for this phrase are as follows, do any of these match your sentence better?"),
+      
+      tableOutput("view")
+
+          )
+  )
+))
